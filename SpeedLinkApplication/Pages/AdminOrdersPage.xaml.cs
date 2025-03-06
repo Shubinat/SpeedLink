4c4816de0e5fb45147542c289b3eb1e0c58c9d65
Shubinat <shubinat729@gmail.com>
2025-03-06 23:54:45 +0300

    Excel генерация

--- SpeedLinkApplication/Pages/AdminOrdersPage.xaml.cs
+++ SpeedLinkApplication/Pages/AdminOrdersPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace SpeedLinkApplication.Pages
 {
@@ -51,6 +52,7 @@ namespace SpeedLinkApplication.Pages
                 orders = orders.Where(x => x.OrderDateTime.Date <= DateToPicker.SelectedDate).ToList();
 
             ChartButton.IsEnabled = orders.Count > 0;
+            ExcelButton.IsEnabled = orders.Count > 0;
             LViewOrders.ItemsSource = orders.OrderByDescending(x => x.OrderDateTime).ToList();
         }
 
@@ -102,6 +104,87 @@ namespace SpeedLinkApplication.Pages
 
                 }
             }
+        }
+
+        private void ExcelButton_Click(object sender, RoutedEventArgs e)
+        {
+            var orders = LViewOrders.ItemsSource as List<Order>;
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Excel.Worksheet sheet = workbook.Worksheets[1];
+
+
+            sheet.Cells[1, 1] = "Дата и время";
+            sheet.Range[sheet.Cells[1, 1], sheet.Cells[2, 1]].Merge();
+            sheet.Cells[1, 2] = "Услуга";
+            sheet.Range[sheet.Cells[1, 2], sheet.Cells[2, 2]].Merge();
+            sheet.Cells[1, 3] = "Стоимость услуги";
+            sheet.Range[sheet.Cells[1, 3], sheet.Cells[2, 3]].Merge();
+            Excel.Range eqHeader = sheet.Range[sheet.Cells[1, 4], sheet.Cells[1, 6]];
+            eqHeader.Merge();
+            eqHeader.Value = "Оборудование";
+            sheet.Cells[2, 4] = "Название";
+            sheet.Cells[2, 5] = "Цена";
+            sheet.Cells[2, 6] = "Количество";
+            sheet.Cells[1, 7] = "Сумма";
+            sheet.Range[sheet.Cells[1, 7], sheet.Cells[2, 7]].Merge();
+            sheet.Cells[1, 8] = "ИТОГО";
+            sheet.Range[sheet.Cells[1, 8], sheet.Cells[2, 8]].Merge();
+
+            Excel.Range mainHeader = sheet.Range[sheet.Cells[1, 1], sheet.Cells[2, 8]];
+            mainHeader.Interior.Color = Excel.XlRgbColor.rgbDeepSkyBlue;
+            mainHeader.Font.Color = Excel.XlRgbColor.rgbWhite;
+            mainHeader.Font.Bold = true;
+            mainHeader.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            var rowIndex = 3;
+            foreach (var order in orders)
+            {
+                sheet.Cells[rowIndex, 1] = order.OrderDateTime.ToString("dd.MM.yyyy HH:mm");
+                sheet.Cells[rowIndex, 2] = order.Service.Name;
+                sheet.Cells[rowIndex, 3] = order.Service.Price;
+                var startIndex = rowIndex;
+                if (order.OrderEquipment.ToList().Count > 0)
+                {
+                    foreach (var item in order.OrderEquipment.ToList())
+                    {
+                        sheet.Cells[rowIndex, 4] = item.Equipment.Name;
+                        sheet.Cells[rowIndex, 5] = item.Equipment.Price;
+                        sheet.Cells[rowIndex, 6] = item.Amount;
+                        sheet.Cells[rowIndex, 7] = $"=F{rowIndex}*E{rowIndex}";
+                        rowIndex++;
+                    }
+
+                }
+                else
+                {
+                    Excel.Range eqRange = sheet.Range[sheet.Cells[rowIndex, 4], sheet.Cells[rowIndex, 6]];
+                    eqRange.Merge();
+                    eqRange.Value = "Нет оборудования";
+                    sheet.Cells[rowIndex, 7] = " - ";
+                    rowIndex++;
+                }
+                sheet.Cells[rowIndex - 1, 8] = $"=SUM(G{startIndex}:G{rowIndex - 1}) + C{startIndex}";
+                sheet.Range[sheet.Cells[startIndex, 1], sheet.Cells[rowIndex - 1, 1]].Merge();
+                sheet.Range[sheet.Cells[startIndex, 2], sheet.Cells[rowIndex - 1, 2]].Merge();
+                sheet.Range[sheet.Cells[startIndex, 3], sheet.Cells[rowIndex - 1, 3]].Merge();
+                sheet.Range[sheet.Cells[startIndex, 8], sheet.Cells[rowIndex - 1, 8]].Merge();
+            }
+            Excel.Range sumHeader = sheet.Range[sheet.Cells[rowIndex, 1], sheet.Cells[rowIndex, 7]];
+                sumHeader.Merge();
+            sumHeader.Font.Bold = true;
+            sumHeader.HorizontalAlignment = Excel.XlHAlign.xlHAlignRight;
+            sumHeader.Value = "ИТОГО ПО ВСЕМ ЗАЯВКАМ:";
+            sheet.Cells[rowIndex, 8] = $"=SUM(H3:H{rowIndex - 1})";
+            sheet.Columns.AutoFit();
+            sheet.UsedRange.Borders[Excel.XlBordersIndex.xlInsideHorizontal].LineStyle =
+            sheet.UsedRange.Borders[Excel.XlBordersIndex.xlInsideVertical].LineStyle =
+            sheet.UsedRange.Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle =
+            sheet.UsedRange.Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle =
+            sheet.UsedRange.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle =
+            sheet.UsedRange.Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle =
+            Excel.XlLineStyle.xlContinuous;
+            sheet.Columns.AutoFit();
+            app.Visible = true;
         }
     }
 }
